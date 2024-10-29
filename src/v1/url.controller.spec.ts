1a80524dcd7600ca95dd7f16b858a390fc71f8ad
Madeee1 <madeee@duck.com>
2024-10-29 18:53:10 +0800

    feat: initial unit testing file

---
+++ src/v1/url.controller.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { V1UrlController } from './url.controller';
+import { V1UrlService } from './url.service';
+
+describe('AppController', () => {
+  let v1UrlController: V1UrlController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [V1UrlController],
+      providers: [V1UrlService],
+    }).compile();
+
+    v1UrlController = app.get<V1UrlController>(V1UrlController);
+  });
+
+  // Test the POST method
+  describe('createUrl', () => {
+    it('should return the long URL and short URL', async () => {
+      const longUrl = 'https://www.npmjs.com';
+      const customShortUrl = 'npm';
+      const response = await v1UrlController.createUrl({
+        longUrl,
+        customShortUrl,
+      });
+      expect(response).toEqual({ longUrl, shortUrl: customShortUrl });
+    });
+
+    it('should return the long URL and the same shortUrl', async () => {
+      const longUrl = 'https://www.npmjs.com';
+      const response = await v1UrlController.createUrl({ longUrl });
+      expect(response.longUrl).toEqual(longUrl);
+      expect(response.shortUrl).toEqual('npm');
+    });
+
+    it('should return the long URL and a unique short URL, 6 characters in length', async () => {
+      const longUrl = 'https://www.github.com';
+      const response = await v1UrlController.createUrl({ longUrl });
+      expect(response.longUrl).toEqual(longUrl);
+      expect(response.shortUrl.length).toEqual(6);
+    });
+  });
+});
